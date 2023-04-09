73fab5559970c163665b512eba0203cb610beca6
daguang123456 <poiuylkjhgmnbvc12@outlook.com>
2023-04-09 03:37:33 -0700

    update

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import datetime
 import webbrowser
 import openai
 from openai.error import InvalidRequestError
+import requests
 
 # authentification
 with open('./bla.yaml') as file:
@@ -24,66 +25,89 @@ authenticator = stauth.Authenticate(
 name, authentication_status, username = authenticator.login('Login', 'main')
 if authentication_status:
     authenticator.logout('Logout', 'main')
-    # page = st.sidebar.selectbox("探索或预测", ("苹果病分类",
-    #     "葡萄病分类",
-    #     "马铃薯病分类",
-    #     "番茄病分类"))
 
-    # if page == "苹果病分类":
-    st.title("使用 DALL-E 生成图像")
-    st.write("教程[link](https://learndataanalysis.org/source-code-use-ai-to-create-images-with-python-tutorial-for-beginners-openai-dall-e-api/)")
-    def generate_image(prompt, num_image=1, size='512x512', output_format='url'):
-        """
-        params:
-            prompt (str):
-            num_image (int):
-            size (str):
-            output_format (str):
-        """
-        try:
-            images = []
-            response = openai.Image.create(
-                prompt=prompt,
-                n=num_image,
-                size=size,
-                response_format=output_format
-            )
-            if output_format == 'url':
-                for image in response['data']:
-                    images.append(image.url)
-            elif output_format == 'b64_json':
-                for image in response['data']:
-                    images.append(image.b64_json)
-            return {'created': datetime.datetime.fromtimestamp(response['created']), 'images': images}
-        except InvalidRequestError as e:
-            print(e)
+    page = st.sidebar.selectbox("探索或预测", ("DeepAI",
+        "OpenAI_Dall-E",))
 
-    # config = configparser.ConfigParser() 
-    # config.read('credential.ini')
-    # API_KEY = config['openai']['APIKEY']
-    openai.api_key = st.secrets["OPENAI_KEY"]
+    if page == "DeepAI":
+        st.title("使用 DeepAI 生成图像")
+        st.write("教程[link](https://deepai.org/machine-learning-model/text2img)")
 
-    SIZES = ('1024x1024', '512x512', '256x256')
 
-    # generate images (url outputs)
-    prompt = st.text_input("Prompt", value="这里输入...")
-    sizz = st.select_slider("大小", options=(['1024x1024', '512x512', '256x256']))
-    # st.write(sizz)
-    if st.button("生成"):
-        with st.spinner("生成图像..."):
-            response = generate_image(prompt, num_image=1, size=SIZES[0])
-            response['created']
-            images = response['images']
-            for image in images:
-                webbrowser.open(image)
-                st.write("图片网址[link]("+image+")")
+        prompt = st.text_input("Prompt", value="这里输入...")
+        # sizz = st.select_slider("大小", options=(['1024x1024', '512x512', '256x256']))
+        # st.write(sizz)
+        if st.button("生成"):
+            with st.spinner("生成图像..."):
+                r = requests.post(
+                    "https://api.deepai.org/api/cute-creature-generator",
+                    data={
+                        'text': prompt,
+                    },
+                    headers={'api-key': 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K'}
+                )
+                print(r.json())
+                st.write("图片网址[link]("+r.json()["output_url"]+")")
 
-            ## generate images (byte output)
-            # response = generate_image('San Francisco and Chicago mixed', num_image=2, size=SIZES[1], output_format='b64_json')
-            # prefix = 'demo'
-            # for indx, image in enumerate(response['images']):
-            #     with open(f'{prefix}_{indx}.jpg', 'wb') as f:
-            #         f.write(b64decode(image))
+    elif page == "OpenAI_Dall-E":
+        st.title("使用 DALL-E 生成图像")
+        st.write("教程[link](https://learndataanalysis.org/source-code-use-ai-to-create-images-with-python-tutorial-for-beginners-openai-dall-e-api/)")
+        def generate_image(prompt, num_image=1, size='512x512', output_format='url'):
+            """
+            params:
+                prompt (str):
+                num_image (int):
+                size (str):
+                output_format (str):
+            """
+            try:
+                images = []
+                response = openai.Image.create(
+                    prompt=prompt,
+                    n=num_image,
+                    size=size,
+                    response_format=output_format
+                )
+                if output_format == 'url':
+                    for image in response['data']:
+                        images.append(image.url)
+                elif output_format == 'b64_json':
+                    for image in response['data']:
+                        images.append(image.b64_json)
+                return {'created': datetime.datetime.fromtimestamp(response['created']), 'images': images}
+            except InvalidRequestError as e:
+                print(e)
+
+        # config = configparser.ConfigParser() 
+        # config.read('credential.ini')
+        # API_KEY = config['openai']['APIKEY']
+        openai.api_key = st.secrets["OPENAI_KEY"]
+
+        SIZES = ('1024x1024', '512x512', '256x256')
+
+        # generate images (url outputs)
+        prompt = st.text_input("Prompt", value="这里输入...")
+        sizz = st.select_slider("大小", options=(['1024x1024', '512x512', '256x256']))
+        # st.write(sizz)
+        if st.button("生成"):
+            with st.spinner("生成图像..."):
+                response = generate_image(prompt, num_image=1, size=SIZES[0])
+                response['created']
+                images = response['images']
+                for image in images:
+                    webbrowser.open(image)
+                    st.write("图片网址[link]("+image+")")
+
+                ## generate images (byte output)
+                # response = generate_image('San Francisco and Chicago mixed', num_image=2, size=SIZES[1], output_format='b64_json')
+                # prefix = 'demo'
+                # for indx, image in enumerate(response['images']):
+                #     with open(f'{prefix}_{indx}.jpg', 'wb') as f:
+                #         f.write(b64decode(image))
+
+
+
+
 
 
 elif authentication_status == False:
